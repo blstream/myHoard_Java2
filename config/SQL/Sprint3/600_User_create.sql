0b374730fd85c4057ac546a5895f6d77fb987658
z0lfik <rafal.olichwer@gmail.com>
2014-03-16 20:28:30 +0100

    poprawka sqla

--- config/SQL/Sprint3/600_User_create.sql
+++ config/SQL/Sprint3/600_User_create.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS User (
         id INT AUTO_INCREMENT, 
 	username varchar(50) CHARACTER SET utf8 COLLATE utf8_polish_ci,
 	mail VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
-        password VARCHAR(30) NOT NULL,
+        password VARCHAR(80) NOT NULL,
 	
 	UNIQUE(username),
         UNIQUE(mail),
